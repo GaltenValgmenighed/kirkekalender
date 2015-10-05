751a894c67395c6398e334f3e3a6f5fff5cd1df3
Klaus Hebsgaard <klaus@hebsgaard.dk>
2015-10-05 15:15:09 +0200

    refactor

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,18 +1,27 @@
 class WelcomeController < ApplicationController
   def index
-
     gava_url = 'https://www.google.com/calendar/ical/r9q5uk26psb23comadtso1isq0%40group.calendar.google.com/public/basic.ics'
     sgim_url = "https://www.google.com/calendar/ical/o1fv0807npa9acc1afas1cv50g%40group.calendar.google.com/public/basic.ics"
-    find_calendars = IFindCalendars.new
-    gava_cal = find_calendars.retrieve_calendar gava_url
-    sgim_cal = find_calendars.retrieve_calendar sgim_url
 
-    gava_events = gava_cal.events.map { |evt| evt.custom_properties["source"]="GAVA";evt}
-    sgim_events = sgim_cal.events.map { |evt| evt.custom_properties["source"]="SGIM";evt}
-    @events = gava_events
-    @events.concat(sgim_events)
+    @events = retrieve_calendar_events(gava_url, "GAVA")
+    @events.concat(retrieve_calendar_events(sgim_url, "SGIM"))
     @events = @events
     .sort { |x,y| x.dtstart <=> y.dtstart}
       .select {|evt| evt.dtstart > Time.now}
+  end
+
+  private
+
+  def find_calendars
+    @find_calendars ||= IFindCalendars.new
+  end
+
+  def retrieve_calendar_events(url, name)
+    cal = find_calendars.retrieve_calendar url
+    events = cal.events.map do
+      |evt| evt.custom_properties["source"] = name
+      evt
+    end
+    events
   end
 end
