aa715f1c368dd47f57fb6682883f1397ec855f8f
Klaus Hebsgaard <klaus@hebsgaard.dk>
2015-10-15 08:57:03 +0200

    make it possible to create and change models in activeadmin

--- app/admin/calendar.rb
+++ app/admin/calendar.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register Calendar do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :list, :of, :attributes, :on, :model
+ permit_params :ics_url, :name, :info_url
 #
 # or
 #
